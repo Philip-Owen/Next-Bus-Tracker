24c2acd5a1964a7d754ae0bd87c5078b34e441c4
philip-owen <owen.philipb@gmail.com>
2021-05-23 14:29:49 -0500

    added mock data and test for stops function

---
+++ test/stops.test.js
@@ -0,0 +1,17 @@
+const expect = require("chai").expect;
+const nock = require("nock");
+
+const response = require("./direction.mock");
+const getStop = require("../nextbus").getStop;
+
+describe("Testing stops", () => {
+  nock("http://svc.metrotransit.org/NexTrip")
+    .get("/Stops/901/0")
+    .reply(200, response);
+
+  it("Fetches Blue line stops", () => {
+    return getStop("28th Ave Station", "901", "0").then((response) => {
+      expect(response).to.equal("28AV");
+    });
+  });
+});
