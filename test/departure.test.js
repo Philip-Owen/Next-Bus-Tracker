3978feab18fb7864040aa78d23d0516fbeb274bb
philip-owen <owen.philipb@gmail.com>
2021-05-23 16:55:20 -0500

    added additional mock data and test set to departure function

--- test/departure.test.js
+++ test/departure.test.js
@@ -1,17 +1,30 @@
 const expect = require("chai").expect;
 const nock = require("nock");
 
-const response = require("./departure.mock");
+const actual = require("./departure-actual.mock");
+const estimated = require("./departure-estimated.mock");
 const getDeparture = require("../nextbus").getDeparture;
 
-describe("Testing departure", () => {
+describe("Testing departure actual", () => {
   nock("https://svc.metrotransit.org/NexTrip")
     .get("/901/0/28AV")
-    .reply(200, response);
+    .reply(200, actual);
 
   it("Fetches Blue line departure", () => {
     return getDeparture("901", "0", "28AV").then((response) => {
-      expect(response).to.have.property("DepartureText");
+      expect(response).to.be.equal("3 Min");
+    });
+  });
+});
+
+describe("Testing departure estimated", () => {
+  nock("https://svc.metrotransit.org/NexTrip")
+    .get("/901/0/28AV")
+    .reply(200, estimated);
+
+  it("Fetches Blue line departure", () => {
+    return getDeparture("901", "0", "28AV").then((response) => {
+      expect(response).to.be.equal("Estimated departure 3:36");
     });
   });
 });
