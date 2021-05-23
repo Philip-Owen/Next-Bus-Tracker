c274995a293a97d93a0b13abf522c4c5290db1d6
philip-owen <owen.philipb@gmail.com>
2021-05-23 13:43:48 -0500

    initial commit. listed out steps to aid in developling logic.

---
+++ nextbus.js
@@ -0,0 +1,13 @@
+// Destructure Route, Stop, Direction from process.argv
+
+// Get routes and filter object to find route specified in args
+// Return Route ID
+
+// Get a routes directions by specifying the route ID and filter based on direction specified in args
+// Return direction value
+
+// Get route stops by specifying route ID + direction and filter and filter based on stop specified in args
+// Return Stop value
+
+// Get departure details by specifying route ID + direction + stop
+// Return DepartureText from first object in array
