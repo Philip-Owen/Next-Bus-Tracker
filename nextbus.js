3638cbb19201764958298ac440acf4314dd316f3
philip-owen <owen.philipb@gmail.com>
2021-05-23 14:08:47 -0500

    created api call helper, and getRoutes function. tests pass.

--- nextbus.js
+++ nextbus.js
@@ -1,8 +1,25 @@
+const fetch = require("node-fetch");
+
 // Destructure Route, Stop, Direction from process.argv
+
+// API Call handler
+const apiFetch = async (endpoint) => {
+  const res = await fetch(`https://svc.metrotransit.org/NexTrip/${endpoint}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  return await res.json();
+};
 
 // Get routes and filter object to find route specified in args
 // Return Route ID
-
+const getRoutes = async (route) => {
+  const data = await apiFetch("Routes");
+  const routeId = data.filter((r) => r.Description === route);
+  return routeId[0].Route;
+};
 // Get a routes directions by specifying the route ID and filter based on direction specified in args
 // Return direction value
 
@@ -11,3 +28,5 @@
 
 // Get departure details by specifying route ID + direction + stop
 // Return DepartureText from first object in array
+
+module.exports = { getRoutes };
